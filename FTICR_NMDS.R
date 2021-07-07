ed64864ce17923661b8e285def2d67e33440d9db
jimjunker1 <jrjunker@mtu.edu>
2021-07-07 08:24:47 -0500

    working to update FTICRms vankrev function and run NMDS. Need to optimize the distance matrices better.

--- FTICR_NMDS.R
+++ FTICR_NMDS.R
@@ -1,102 +1,88 @@
+source("packages.R")
 
-library(ggplot2)
-library(tidyr)
-library(plyr)
-library(dplyr)
-library(reshape2)
-library(vegan)
-library(ggrepel)
-library(labdsv)
+# library(ggplot2)
+# library(tidyr)
+# library(plyr)
+# library(dplyr)
+# library(reshape2)
+# library(vegan)
+# library(ggrepel)
+# library(labdsv)
 
-setwd("")
+# !++++    HELPER FUNCTIONS    ++++!#
+#' @title vankrev_class
+#' @param data data.frame or coercible; long data frame of molecular forms
+#' @param modify logical; if TRUE (default) the function will modify columns
+#'  'character', 'hetero', 'type' 
+#'
+#'
+#'
+
+vankrev_class = function(data, type_list = NULL, modify = TRUE,...){
+  
+  data$pres <- 1
+  
+  '%ni%' = Negate('%in%')
+  
+   cols = c("character", "hetero", "type")
+   cols_to_add = setdiff(cols, names(data))
+   
+   data[cols_to_add] <- NA_character_
+   
+   x = data %>% 
+     dplyr::mutate(character = case_when(between(OC,0,0.29) & between(HC, 1.6,2.5) ~ "lipid",
+                                         between(OC,0.29, 0.6) & between(HC,1.5,2.5) ~ "protein",
+                                         between(OC, 0.6,1.2) & between(HC, 1.5,2.5) ~ "ascarb",
+                                         between(OC, 0,0.29) & between(HC, 1,1.6) ~ "uhc",
+                                         between(OC, 0,0.4) & between(HC, 0,0.7) ~ "ca",
+                                         between(OC, 0.29, 0.65) & between(HC, 0.7, 1.5) ~ "lignin",
+                                         between(OC, 0.65, 1.2) & between(HC, 0.5, 1.5) ~ "tannin",
+                                         TRUE ~ "unclass"),
+                   hetero = case_when(O == 0 & N == 0 & S == 0 ~ "CH",
+                                      O != 0 & N == 0 & S == 0 ~ "CHO",
+                                      O == 0 & N == 1 & S == 0 ~ "CHN",
+                                      O == 1 & N == 1 & S == 0 ~ "CHNO",
+                                      O != 1 & N == 1 & S == 0 ~ "CHON1",
+                                      O != 0 & N == 1 & S == 1 ~ "CHON1S1",
+                                      O != 0 & N == 1 & S == 1 ~ "CHOS1",
+                                      O != 0 & N == 2 & S == 0 ~ "CHON2",
+                                      N == 2 & S == 1 ~ "CHON2S1",
+                                      N == 3 ~ "CHON3",
+                                      TRUE ~ NA_character_),
+                   type = recode(sample, !!!type_list))
+   
+   return(x)
+   
+}
+# !++++   END HELPER FUNCTIONS    ++++!#
+
 
 ######   
-ms_full = read.csv(file = "F:/Energy & Fuels Alan Marshall Manuscript/NMDS All Samples/NMDS_MRB.csv", T, stringsAsFactors = F)
-samps = list("TOP", "Y01", "Y06", "FS1", "FS2", "FS3", "FS6","SC", "LG", "LL", "LP", "Miss", "ESFA", "GG1", "GG2", "GG3", "GG4", "POXC2", "POXC3", "POXC4","POXC5","POXC6", "POXC7", "AGC15", "BGC15", "AGC16", "BGC16", "D2G", "D2L", "D2P", "LSW", "PLFA", "SRFA", "SRNOM", "PG", "PO", "PP", "MO", "HF")
-colnames(MRB) = c("sample", "C", "H","N","O","S","OC","HC","Molform", "chemsupp")
-#ms_full = rbind(ms_full, ms4)
-
-ms_full$type = NA
-ms_full$hetero = NA
-ms_full$character = NA
-ms_full$samp = ms_full$sample
-ms_full$pres <- 1
-
-FW_fix = which(ms_full$sample == "SRFA" | ms_full$sample == "SRNOM" | ms_full$sample == "AGC15" | 
-                 ms_full$sample == "BGC15" | ms_full$sample == "TOP" | ms_full$sample == "Y01" |
-                 ms_full$sample == "FS1" | ms_full$sample == "FS2" | ms_full$sample == "FS3" | 
-                 ms_full$sample == "FS6" | ms_full$sample == "SC" | ms_full$sample == "LG" |
-                 ms_full$sample == "LL" | ms_full$sample == "LP" | ms_full$sample == "Miss" | 
-                 ms_full$sample == "GG1" | ms_full$sample == "GG2" | ms_full$sample == "GG3" |
-                 ms_full$sample == "GG4" | ms_full$sample == "D2G" | ms_full$sample == "D2L" | 
-                 ms_full$sample == "D2P" | ms_full$sample == "PLFA")
-ms_full[FW_fix, "type"] = "FW"
-
-T_fix = which(ms_full$sample == "ESFA" | ms_full$sample == "PG" | ms_full$sample == "PO" |
-                ms_full$sample == "PP" | ms_full$sample == "POXC2" | ms_full$sample == "POXC3" | 
-                ms_full$sample == "POXC4" | ms_full$sample == "POXC5" | 
-                ms_full$sample == "POXC6" | ms_full$sample == "POXC7")
-ms_full[T_fix, "type"] = "T"
-
-M_fix = which(ms_full$sample == "LSW" | ms_full$sample == "MO" | ms_full$sample == "HF")
-ms_full[M_fix, "type"] = "M"
+ms_full = read.csv(file = "./data/NMDS_MRB.csv", T, stringsAsFactors = F) %>%
+  dplyr::mutate(pres == 1, type = NA_character_, hetero = NA_character_, character = NA_character_)
+# samps = list("TOP", "Y01", "Y06", "FS1", "FS2", "FS3", "FS6","SC", "LG", "LL", "LP", "Miss", "ESFA", "GG1", "GG2", "GG3", "GG4", "POXC2", "POXC3", "POXC4","POXC5","POXC6", "POXC7", "AGC15", "BGC15", "AGC16", "BGC16", "D2G", "D2L", "D2P", "LSW", "PLFA", "SRFA", "SRNOM", "PG", "PO", "PP", "MO", "HF")
 
 
-lipid_fix = which(ms_full$OC > 0 & ms_full$OC <= 0.29 & ms_full$HC >= 1.6 & ms_full$HC <= 2.5)
-ms_full[lipid_fix, "character"] = "lipid";rm(lipid_fix)
-protein_fix = which(ms_full$OC > 0.29 & ms_full$OC <= 0.6 & ms_full$HC >= 1.5 & ms_full$HC <= 2.5)
-ms_full[protein_fix, "character"] = "protein";rm(protein_fix)
-ascarb_fix = which(ms_full$OC > 0.6 & ms_full$OC <= 1.2 & ms_full$HC >= 1.5 & ms_full$HC <= 2.5)
-ms_full[ascarb_fix, "character"] = "ascarb";rm(ascarb_fix)
-uhc_fix = which(ms_full$OC > 0 & ms_full$OC <= 0.29 & ms_full$HC >= 1 & ms_full$HC <= 1.6)
-ms_full[uhc_fix, "character"] = "uhc";rm(uhc_fix)
-ca_fix = which(ms_full$OC > 0 & ms_full$OC <= 0.4 & ms_full$HC >= 0 & ms_full$HC <= 0.7)
-ms_full[ca_fix, "character"] = "ca";rm(ca_fix)
-lignin_fix = which(ms_full$OC > 0.29 & ms_full$OC <= 0.65 & ms_full$HC >= 07 & ms_full$HC <= 1.5)
-ms_full[lignin_fix, "character"] = "lignin";rm(lignin_fix)
-tannin_fix = which(ms_full$OC > 0.65 & ms_full$OC <= 1.2 & ms_full$HC >= 05 & ms_full$HC <= 1.5)
-ms_full[tannin_fix, "character"] = "tannin";rm(tannin_fix)
-unclass_fix = which(is.na(ms_full$character))
-ms_full[unclass_fix, "character"] = "unclass";rm(unclass_fix)
+fw_fix <- c("SRFA", "SRNOM", "AGC15", "BGC15", "TOP", "Y01", "FS1", "FS2", "FS3",
+            "FS6", "SC", "LG","LL", "LP", "Miss", "GG1", "GG2", "GG3","GG4",
+            "D2G", "D2L", "D2P", "PLFA") %>% setNames(rep("fw", length(.)),.)
 
-CH.fix = which(ms_full$O == 0 & ms_full$N == 0 & ms_full$S == 0)
-ms_full[CH.fix, "grp"] = "CH"
-rm(CH.fix)
-CHO.fix = which(ms_full$N == 0 & ms_full$S == 0)
-ms_full[CHO.fix, "grp"] = "CHO"
-rm(CHO.fix)
-CHN.fix = which(ms_full$O == 0 & ms_full$N == 1 & ms_full$S == 0)
-ms_full[CHN.fix, "grp"] = "CHN"
-rm(CHN.fix)
-CHNO.fix = which(ms_full$O == 1 & ms_full$N == 1 & ms_full$S == 0)
-ms_full[CHNO.fix, "grp"] = "CHNO"
-rm(CHNO.fix)
-CHO.fix = which(ms_full$N == 0 & ms_full$S == 0)
-ms_full[CHO.fix, "hetero"] = "CHO"
-rm(CHO.fix)
-CHON.fix = which(ms_full$N == 1 & ms_full$S == 0)
-ms_full[CHON.fix, "hetero"] = "CHON1"
-rm(CHON.fix)
-CHONS.fix =which(ms_full$N == 1 & ms_full$S == 1)
-ms_full[CHONS.fix, "hetero"] = "CHON1S1"
-rm(CHONS.fix)
-CHOS.fix = which(ms_full$N == 0 & ms_full$S == 1)
-ms_full[CHOS.fix, "hetero"] = "CHOS1"
-rm(CHOS.fix)
-CHON2.fix = which(ms_full$N == 2 & ms_full$S == 0)
-ms_full[CHON2.fix, "hetero"] = "CHON2"
-rm(CHON2.fix)
-CHON2S.fix = which(ms_full$N == 2 & ms_full$S == 1)
-ms_full[CHON2S.fix, "hetero"] = "CHON2S1"
-rm(CHON2S.fix)
-CHON3.fix = which(ms_full$N == 3 & ms_full$S == 0)
-ms_full[CHON3.fix, "hetero"] = "CHON3"
-rm(CHON3.fix)
+terr_fix =   c("ESFA", "PG", "PO", "PP", "POXC2", "POXC3", "POXC4", "POXC5",
+               "POXC6", "POXC7") %>% setNames(rep("terr", length(.)),.) 
+
+marine_fix = c("LSW", "MO", "HF") %>% setNames(rep("marine", length(.)),.)
+
+full_fix_list = do.call(c, list(fw_fix, terr_fix, marine_fix))
+
+# debugonce(vankrev_class)
+ms_full = vankrev_class(ms_full, type_list = full_fix_list)
+
 
 ms_full$Dup = duplicated(ms_full)
 length(which(ms_full$Dup == T))
 ms_full = subset(ms_full, ms_full$Dup == F)
-which(ms_full$chem.spp == "C24H33N1O8S0")
+
+
 ######
 NMDSvectors <- plyr::ddply(ms_full, c("sample"), summarize, OCmean = mean(OC), HCmean = mean(HC), lignin = length(which(character == "lignin")), 
                      uhc = length(which(character == "uhc")), protein = length(which(character == "protein")), ca = length(which(character == "ca")),
@@ -105,24 +91,24 @@ NMDSvectors <- plyr::ddply(ms_full, c("sample"), summarize, OCmean = mean(OC), H
                      CHON2 = length(which(hetero == "CHON2")), CHON2S1 = length(which(hetero == "CHON2S1")), CHOS1 = length(which(hetero == "CHOS1")), CH = length(which(hetero == "CH")), 
                      CHN = length(which(hetero == "CHN")), CHON3 = length(which(hetero == "CHON3"))) 
 NMDSvectors$type = NA
-FW_fix = which(ms_full$sample == "SRFA" | ms_full$sample == "SRNOM" | ms_full$sample == "AGC15" | 
-                 ms_full$sample == "BGC15" | ms_full$sample == "TOP" | ms_full$sample == "Y01" |
-                 ms_full$sample == "FS1" | ms_full$sample == "FS2" | ms_full$sample == "FS3" | 
-                 ms_full$sample == "FS6" | ms_full$sample == "SC" | ms_full$sample == "LG" |
-                 ms_full$sample == "LL" | ms_full$sample == "LP" | ms_full$sample == "Miss" | 
-                 ms_full$sample == "GG1" | ms_full$sample == "GG2" | ms_full$sample == "GG3" |
-                 ms_full$sample == "GG4" | ms_full$sample == "D2G" | ms_full$sample == "D2L" | 
-                 ms_full$sample == "D2P" | ms_full$sample == "PLFA")
-ms_full[FW_fix, "type"] = "FW"
-
-T_fix = which(ms_full$sample == "ESFA" | ms_full$sample == "PG" | ms_full$sample == "PO" |
-                ms_full$sample == "PP" | ms_full$sample == "POXC2" | ms_full$sample == "POXC3" | 
-                ms_full$sample == "POXC4" | ms_full$sample == "POXC5" | 
-                ms_full$sample == "POXC6" | ms_full$sample == "POXC7")
-ms_full[T_fix, "type"] = "T"
-
-M_fix = which(ms_full$sample == "LSW" | ms_full$sample == "MO" | ms_full$sample == "HF")
-ms_full[M_fix, "type"] = "M"
+# FW_fix = which(ms_full$sample == "SRFA" | ms_full$sample == "SRNOM" | ms_full$sample == "AGC15" | 
+#                  ms_full$sample == "BGC15" | ms_full$sample == "TOP" | ms_full$sample == "Y01" |
+#                  ms_full$sample == "FS1" | ms_full$sample == "FS2" | ms_full$sample == "FS3" | 
+#                  ms_full$sample == "FS6" | ms_full$sample == "SC" | ms_full$sample == "LG" |
+#                  ms_full$sample == "LL" | ms_full$sample == "LP" | ms_full$sample == "Miss" | 
+#                  ms_full$sample == "GG1" | ms_full$sample == "GG2" | ms_full$sample == "GG3" |
+#                  ms_full$sample == "GG4" | ms_full$sample == "D2G" | ms_full$sample == "D2L" | 
+#                  ms_full$sample == "D2P" | ms_full$sample == "PLFA")
+# ms_full[FW_fix, "type"] = "FW"
+# 
+# T_fix = which(ms_full$sample == "ESFA" | ms_full$sample == "PG" | ms_full$sample == "PO" |
+#                 ms_full$sample == "PP" | ms_full$sample == "POXC2" | ms_full$sample == "POXC3" | 
+#                 ms_full$sample == "POXC4" | ms_full$sample == "POXC5" | 
+#                 ms_full$sample == "POXC6" | ms_full$sample == "POXC7")
+# ms_full[T_fix, "type"] = "T"
+# 
+# M_fix = which(ms_full$sample == "LSW" | ms_full$sample == "MO" | ms_full$sample == "HF")
+# ms_full[M_fix, "type"] = "M"
 
 hist(NMDSvectors$lignin);shapiro.test(NMDSvectors$lignin);qqnorm(NMDSvectors$lignin)
 hist(NMDSvectors$uhc);shapiro.test(NMDSvectors$uhc);qqnorm(NMDSvectors$uhc)
@@ -131,7 +117,26 @@ hist(NMDSvectors$ca);shapiro.test(NMDSvectors$ca)
 hist(NMDSvectors$tannin);shapiro.test(NMDSvectors$tannin)
 
 #######run to format the data frame for Primer
-ms.spp.long <- reshape2::dcast(ms_full, chemsupp ~ samp, value.var = "pres")
+ms.spp.long <- ms_full  %>%
+  group_by(sample) %>% 
+  unique %>%
+  dplyr::select(sample, chemsupp, pres) %>%
+  pivot_wider(sample, names_from = chemsupp, values_from = pres, values_fill = 0) %>%
+  column_to_rownames("sample") %>% as.matrix
+
+x = vegan::vegdist(ms.spp.long, method = "manhattan", diag = FALSE) %>% as.matrix
+apply(x,1,sum)
+
+hist(x[upper.tri(x)])
+
+write.csv(x, file = "./MRB_vegdist.csv")
+set.seed(123)
+manhattan_1 = vegan::metaMDS(ms.spp.long, diatance = "manhattan", k = 2, try = 1000, center = TRUE,
+                         trymax = 5000, maxit = 5000, autotransform = TRUE)
+
+vegan::stressplot(manhattan_1)
+plot(manhattan_1)
+orditorp(manhattan_1,display="species",cex=1.25,air=0.01)
 
 ms.spp.long[is.na(ms.spp.long)] = 0
 ms.spp.long2 = ms.spp.long[,-1]
